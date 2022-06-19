7b8c6636a8e2a29f79426161056b3763baae2a01
Matvei Gavrilov <baricred@mail.ru>
2022-06-19 19:56:36 +0700

    input_submit

---
+++ Input.php
@@ -0,0 +1,22 @@
+<?php
+require_once 'tag.php';
+class Input extends Tag
+{
+    public function __construct(){
+        parent::__construct('input');
+    }
+    public function open(){
+        $inputName = $this->getAttr('name');
+        // если name был задан у инпута:
+        if ($inputName) {
+            if (isset($_REQUEST[$inputName])) {
+                $value = $_REQUEST[$inputName];
+                $this->setAttr('value', $value);
+            }
+        }
+        return parent::open();
+    }
+    public function __toString(){
+        return $this->open();
+    }
+}
